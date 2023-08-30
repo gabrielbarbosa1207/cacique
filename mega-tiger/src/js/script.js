4e39b1d31ac44d3a3c35654f301d1b364382bc7f
gabrielbarbosaweb <gabrielbarbosasa2@gmail.com>
2023-08-30 00:35:14 -0300

    all

--- mega-tiger/src/js/script.js
+++ mega-tiger/src/js/script.js
@@ -1,46 +1,36 @@
-const counter = document.querySelector("#counter");
-const counterDown = document.querySelector("#counterDown");
+document.querySelector("a").addEventListener("click", function(event) {
+    event.preventDefault(); // prevent the default action (navigation) of the link
 
-let countInit = 1348;
-const countEnd = 8973;
-const durationInSeconds = 6; // Duration for the countUp
+    // Disable the button and change its appearance
+    const button = event.target;
+    button.style.backgroundColor = "gray";
+    button.textContent = "aguarde 30s...";
+    button.removeEventListener("click", arguments.callee);  // Remove the click event listener
 
-const increment = (countEnd - countInit) / (durationInSeconds * 10);
+    // Generate random number between 5 and 45, then append "X" to it
+    let normalValue = Math.floor(Math.random() * (35 - 5 + 1) + 5) + "X";
+    let turboValue = Math.floor(Math.random() * (25 - 5 + 1) + 5) + "X";
 
-function updateCounter() {
-    countInit += increment;
+    // Generate the current time and add 5 minutes
+    let date = new Date();
+    date.setMinutes(date.getMinutes() + 5);
+    let validadeValue = date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0');
 
-    if (countInit >= countEnd) {
-        countInit = countEnd;
-        clearInterval(intervalUp);
-    }
+    // Set the generated values to the spans
+    document.getElementById("normal").textContent = normalValue;
+    document.getElementById("turbo").textContent = turboValue;
+    document.getElementById("validade").textContent = validadeValue;
 
-    // Update the counter's textContent with the new value
-    counter.textContent = Math.round(countInit).toString();
-}
-
-// CountDown Logic
-let countDownInit = 12;
-const countDownEnd = 2;
-const durationInSecondsDown = 15; // Duration for the countDown, you can change this as per requirement
-
-const decrement = (countDownInit - countDownEnd) / (durationInSecondsDown * 10); 
-
-function updateCounterDown() {
-    countDownInit -= decrement;
-
-    if (countDownInit <= countDownEnd) {
-        countDownInit = countDownEnd;
-        clearInterval(intervalDown);
-    }
-
-    // Update the counterDown's textContent with the new value
-    counterDown.textContent = Math.round(countDownInit).toString();
-}
-
-// This will update the counterUp every 100ms (10 times a second) 
-const intervalUp = setInterval(updateCounter, 100);
-
-// This will update the counterDown every 200ms (5 times a second) 
-// Adjust the 200ms to change the frequency for countdown 
-const intervalDown = setInterval(updateCounterDown, 200);
+    // Start the countdown
+    let counter = 30;
+    const interval = setInterval(function() {
+        counter--;
+        button.textContent = "Aguarde " + counter + "s...";
+        if (counter <= 0) {
+            clearInterval(interval);
+            button.style.backgroundColor = "green";
+            button.textContent = "GERAR NOVO SINAL";
+            button.addEventListener("click", arguments.callee);  // Re-add the click event listener
+        }
+    }, 1000);  // This function will be executed every second
+});
